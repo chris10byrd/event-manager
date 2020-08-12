4c8678aa7ca4d4156463a82011ef3f06eb00cc6b
Christian Byrd <chris10byrd@hotmail.com>
2020-08-12 19:41:54 -0400

    Initial Commit with a very simple CLI implemented. Also includes the JSON file for the Google Calendar API.

---
+++ event-manager.py
@@ -0,0 +1,7 @@
+print("Welcome to Event Manager v0.1")
+answer = input("Would you like to create an event? (y/n)\n")
+
+if (answer == "y"):
+    print("Creating event..")
+else:
+    print("Ok. Exiting program now...")
